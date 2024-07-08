8f49fd82d9eefcee94f592f5e66b03e11eeaa3db
Annelies11 <mmahmudi1103@gmail.com>
2024-07-08 12:08:41 +0700

    submit btn

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ const App = () => {
     <section className="section-center">
       <form onSubmit={handleSubmit}>
         {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3 style={{marginBottom: "1.5rem", textAlign: "center"}}>
+          Todo List Using Local Storage
+        </h3>
+        <div className="mb-3 form">
+          <input type="text" className="form-control" placeholder="e.g. Mahmudi" onChange={(e) => setName(e.target.value)} value={name}/>
+          <button type="submit" className="btn btn-success">{isEditing ? "Edit" : "Submit"}</button>
+        </div>
       </form>
     </section>
   )
